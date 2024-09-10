51329fafb3cffd9fc345f11a6d53f533286588b9
williamrygao <wimrygao@gmail.com>
2024-09-09 20:42:38 -0400

    refactored to extract helper method

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+        String[] letters;
+        letters = new String[1];
+        FizzBuzz.main(letters);
+    }
+}
